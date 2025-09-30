1fd770c19cc0528836029fa61966cb0a7abaed99
Micah Bowie <micahbowie20@gmail.com>
2025-09-30 16:26:10 -0500

    chore: update should wording

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,7 +15,7 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
+    it('returns "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
