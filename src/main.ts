2d63785bf4492c6814fab31560c30e2cfb82df24
Micah Bowie <micahbowie20@gmail.com>
2025-09-30 16:06:25 -0500

    Add pre-commit hooks with husky and lint-staged

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,16 @@ import '../instrument';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConsoleLogger } from '@nestjs/common';
+import { GraphQLLogger } from './logger/graphql-logger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: new ConsoleLogger({
       json: true,
     }),
+    bufferLogs: true,
   });
+  app.useLogger(app.get(GraphQLLogger));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
